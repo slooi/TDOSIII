f4b0abd91875e881115c7484f61d3d690deae834
slooi <s.looi2@outlook.com>
2020-11-15 02:47:55 +1300

    Fix problem with start script

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const PORT = 3000;
+
 // Middleware
 app.use(express.static(path.resolve(__dirname, "..", "public")));
 //asd
+
+app.listen(PORT, () => {
+  console.log("Listening on port " + PORT);
+});
